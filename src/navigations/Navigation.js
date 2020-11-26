8c66f7462932c6b51544e65081719f9fa138f6c4
Clavier Eliott <eliott.clavier@students.campus.academy>
2020-11-26 23:25:31 +0100

    Mise à jour de la barre de navigation (changement de bottom vers top navigation bar de material pour permettre le swipe)

--- src/navigations/Navigation.js
+++ src/navigations/Navigation.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import 'react-native-gesture-handler';
 import Message from '../components/Message/Message';
 import ListeMessage from '../components/ListeMessage/ListeMessage';
@@ -9,8 +10,9 @@ import Accueil from '../components/Accueil/Accueil';
 import {Icon} from 'react-native-elements';
 
 import colors from '../config/colors';
+import {color} from 'react-native-reanimated';
 
-const Tab = createMaterialBottomTabNavigator();
+const Tab = createMaterialTopTabNavigator();
 
 class Navigation extends React.Component{
     render(){
@@ -19,16 +21,22 @@ class Navigation extends React.Component{
                 <NavigationContainer>
                     <Tab.Navigator
                     initialRouteName="Home"
-                    barStyle={ styles.bottomBar }
-                    activeColor={ colors.black }
-                    inactiveColor={ colors.inactiveBlack }
-                    labeled={false}
-                    >
+                    tabBarPosition="bottom"
+                    tabBarOptions={{
+                      showIcon: true,
+                      showLabel: false,
+                      activeTintColor: colors.black,
+                      inactiveTintColor: colors.inactiveBlack,
+                      indicatorStyle: {
+                        borderBottomColor: colors.black,
+                        borderBottomWidth: 2,
+                      },
+                    }} >
                         <Tab.Screen
                           name="Home"
                           component={Accueil}
                           options={{
-                              tabBarColor: '#000',
+                              tabBarColor: colors.white,
                               tabBarIcon: ({color, size}) => (
                                 <Icon name="home" color={color} size={size} />
                               )
@@ -37,7 +45,7 @@ class Navigation extends React.Component{
                           name="Liste des Messages"
                           component={ListeMessage}
                           options={{
-                            tabBarColor: '#000',
+                            tabBarColor: colors.white,
                             tabBarIcon: ({color, size}) => (
                               <Icon name="list" color={color} size={size} />
                             )
@@ -46,7 +54,7 @@ class Navigation extends React.Component{
                           name="Message"
                           component={Message}
                           options={{
-                            tabBarColor: '#000',
+                            tabBarColor: colors.white,
                             tabBarIcon: ({color, size}) => (
                               <Icon name="message" color={color} size={size} />
                             )
